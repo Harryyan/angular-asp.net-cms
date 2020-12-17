3f14cc3ccde9232162f7cd15132a7d21b2c72cc7
Harry Yan <harry.yan@plexure.com>
2020-12-18 06:16:55 +1300

    add css

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+      TopNavComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
